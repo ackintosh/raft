a4c6e9a86f668dcdd29a7161c24494292a130e35
akihito.nakano <sora.akatsuki@gmail.com>
2019-09-15 15:30:41 +0900

    Send RequestVote RPC

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,32 @@
 use std::net::{TcpListener, TcpStream};
-use std::io::Read;
+use std::io::{Read, Write};
 use std::time::{Instant, Duration};
 use std::sync::{Arc, RwLock};
 use std::ops::Add;
+
+#[macro_use]
+extern crate serde_derive;
 
 // https://raft.github.io/raft.pdf
 fn main() {
     println!("Hello, Raft!");
     let args: Vec<String> = std::env::args().collect();
     println!("Command line args: {:?}", args);
-    let network = Network::new(&args);
+    let network = Arc::new(Network::new(&args));
     println!("Network: {:?}", network);
+    let node_id = Arc::new(format!("node_{}", network.port));
+    println!("NodeId: {:?}", node_id);
 
     // When servers start up, they begins as followers
     let server_state = Arc::new(RwLock::new(ServerState::new()));
-    let _state = State::new();
+    let state = Arc::new(State::new());
 
     let heartbeat_received_at = Arc::new(RwLock::new(HeartbeatReceivedAt::new()));
 
     let leader_election = LeaderElection::new(
+        node_id.clone(),
+        network.clone(),
+        state.clone(),
         server_state.clone(),
         heartbeat_received_at.clone()
     );
@@ -26,7 +34,7 @@ fn main() {
         leader_election.start();
     });
 
-    let mut rpc_handler = RpcHandler { network: &network };
+    let mut rpc_handler = RpcHandler { network: network.clone() };
     rpc_handler.listen();
 }
 
@@ -40,7 +48,7 @@ impl Network {
     fn new(args: &Vec<String>) -> Self {
         Self {
             port: args[1].clone(),
-            nodes: args[1..].to_vec().into_boxed_slice(),
+            nodes: args[2..].to_vec().into_boxed_slice(),
         }
     }
 }
@@ -96,11 +104,11 @@ impl State {
     }
 }
 
-struct RpcHandler<'a> {
-    network: &'a Network,
+struct RpcHandler {
+    network: Arc<Network>,
 }
 
-impl RpcHandler<'_> {
+impl RpcHandler {
     fn listen(&mut self) {
         let address = format!("127.0.0.1:{}", self.network.port);
         println!("RpcHandler is listening on {}", address);
@@ -116,11 +124,38 @@ impl RpcHandler<'_> {
         let size = stream.read(&mut buffer).unwrap();
         let body = String::from_utf8_lossy(&buffer[..size]).to_string();
 
+        // TODO
         println!("Rpc message body: {}", body);
     }
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+struct RequestVote {
+    // candidate's term
+    term: u64,
+    // candidate requesting vote
+    candidate_id: String,
+    // index of candidate's last log entry
+    last_log_index: u64,
+    // term of candidate's log entry
+    last_log_term: u64,
+}
+
+impl RequestVote {
+    fn new(node_id: Arc<String>, state: Arc<State>) -> Self {
+        Self {
+            term: state.current_term,
+            candidate_id: node_id.to_string(),
+            last_log_index: (state.logs.len() + 1) as u64,
+            last_log_term: 1, // TODO
+        }
+    }
+}
+
 struct LeaderElection {
+    node_id: Arc<String>,
+    network: Arc<Network>,
+    state: Arc<State>,
     server_state: Arc<RwLock<ServerState>>,
     election_timeout: Duration,
     heartbeat_received_at: Arc<RwLock<HeartbeatReceivedAt>>,
@@ -148,17 +183,23 @@ impl HeartbeatReceivedAt {
 
 impl LeaderElection {
     fn new(
+        node_id: Arc<String>,
+        network: Arc<Network>,
+        state: Arc<State>,
         server_state: Arc<RwLock<ServerState>>,
         heartbeat_received_at: Arc<RwLock<HeartbeatReceivedAt>>
     ) -> Self {
         Self {
+            node_id,
+            network,
+            state,
             server_state,
             election_timeout: Duration::from_secs(3), // TODO: Randomize per node
             heartbeat_received_at,
         }
     }
 
-    fn start(self) {
+    fn start(&self) {
         loop {
             let timeout = self.heartbeat_received_at
                 .read()
@@ -167,14 +208,48 @@ impl LeaderElection {
             let now = Instant::now();
 
             if now > timeout {
-                // TODO
                 println!("Receives no communication over a period `election timeout`.");
                 self.server_state.write().unwrap().to_candidate();
 
+                self.start_election();
                 println!("Reset the heartbeat_received_at");
                 self.heartbeat_received_at.write().unwrap().reset();
             } else {
                 std::thread::sleep(timeout - now);
+            }
+        }
+    }
+
+    fn start_election(&self) {
+        println!("The election has been started");
+        let message = serde_json::to_string(
+            &RequestVote::new(
+                self.node_id.clone(),
+                self.state.clone()
+            ))
+            .unwrap();
+        for node in self.network.nodes.iter() {
+            self.request_vote(node, message.as_bytes());
+        }
+    }
+
+    fn request_vote(&self, node: &String, message: &[u8]) -> Result<(), String> {
+        match TcpStream::connect(format!("127.0.0.1:{}", node)) {
+            Ok(mut stream) => {
+                println!("Successfully connected to the node: {:?}", node);
+
+                match stream.write(message) {
+                    Ok(size) => {
+                        println!("Sent {} bytes", size);
+                        Ok(())
+                    }
+                    Err(e) => {
+                        Err(format!("Failed to RequestVote RPC: {:?}", e))
+                    }
+                }
+            }
+            Err(e) => {
+                Err(format!("Failed to connect to the node: {:?}, error: {:?}", node, e))
             }
         }
     }
